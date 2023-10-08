6d963825248e35aafbd865c2e7d340dc8ab02842
vera-kulish <verakoulich@gmail.com>
2023-10-08 19:14:22 +0200

    Homework7 Task2

---
+++ src/Homework7/Task2/Main.java
@@ -0,0 +1,11 @@
+package Homework7.Task2;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        Director director = new Director("Name LastName", 50);
+        director.printOccupation();
+
+    }
+}
